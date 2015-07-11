de02374abf5cea800ea3f6597ccb8684882eea90
Jens Schwehn <jschwehn+github@gmail.com>
2015-07-11 13:29:42 +0200

    Added some checks Made the config more flexible by introducing a environment switch

--- config/config.dist.php
+++ config/config.dist.php
@@ -1,17 +1,21 @@
 <?php
 
-    return [
-        'app.name'              => '<appname>',
+$_cfg = [
+    'app.name' => '<App Name>',
+    'db.host' => 'localhost',
+    'db.user' => '<user>',
+    'db.pass' => '',
+    'db.name' => '<dbname>',
+    'db.charset' => 'utf8mb4',
+    'cookies.secret_key' => 'sdf43rsfdg43453edrfv fvxsfdc',
+    'debug' => true,
+    'templates.path' => '../templates/'
+];
 
-        'db.host'               => 'localhost',
-        'db.user'               => '<user>',
-        'db.pass'               => '',
-        'db.name'               => '<dbname>',
-        'db.charset'            => 'utf8mb4',
+$_dev_cfg = [];
 
-        'cookies.secret_key'    => '<cookie-secret>',
+if ('development' === $environment && (file_exists(__DIR__ . "/config.dev.php") && is_readable(__DIR__ . "/config.dev.php"))) {
+    $_dev_cfg = require __DIR__ . "/config.dev.php";
+}
 
-        'debug'                 => true,
-
-        'templates.path'        => '../templates/'
-    ];
+return array_merge($_cfg, $_dev_cfg);
--- public/index.php
+++ public/index.php
@@ -1,5 +1,26 @@
 <?php
 
+
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    header('Content-type: text/html; charset=utf-8', true, 503);
+    echo '<h2> Error </h2>';
+    echo 'Did not find the autoloading file. Did you run the composer.phar install command?';
+    return;
+}
+if (!file_exists(__DIR__ . '/../config/config.php')) {
+    header('Content-type: text/html; charset=utf-8', true, 503);
+    echo '<h2> Error </h2>';
+    echo 'Did not find a valid config file. Did you forget to rename and change the config.dist.php file?';
+    return;
+}
+if (version_compare(PHP_VERSION, '5.6.0', '<')) {
+    header('Content-type: text/html; charset=utf-8', true, 503);
+    echo '<h2> Error </h2>';
+    echo 'Your PHP Version ' . PHP_VERSION . ' is to old and we are expect at least 5.6.0';
+    return;
+}
+
+$environment = getenv('CODM_ENV') ?: 'production';
 require '../vendor/autoload.php';
 
 $config = require(__DIR__ . '/../config/config.php');
